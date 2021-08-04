f29edfe79114ba26e85b00572251211303f88456
April Long <april11dance@gmail.com>
2021-08-04 15:14:07 -0600

    cleaned file

---
+++ 02-Console/new.js
@@ -0,0 +1,4 @@
+// Simple console.log using Node.js and  terminal/bash.
+// * Inside the new.js file use JavaScript to log the words: "_______ Goes great on.... nothing." (or everything. You choose.)
+// * Then run the program using Node in your terminal/bash window.
+
---
+++ 03-Arguments/arguments.js
@@ -0,0 +1,10 @@
+// process.argv will print out any command line arguments.
+// console.log(process.argv);
+//node executable path
+//1st index contains path to your current file. 
+// the rest contains the argument you're passing. 
+
+// Combines two command line arguments (strings).
+// Use parsefloat to return a number/integar 
+
+// Adds two command line arguments (numbers).
