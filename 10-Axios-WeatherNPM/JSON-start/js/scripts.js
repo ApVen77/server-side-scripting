8ba0cb51890a7b605f21f58ade9b3b3dc73682ae
April Long <april11dance@gmail.com>
2022-01-12 15:08:35 -0700

    fixed the weather icon

--- 10-Axios-WeatherNPM/JSON-start/js/scripts.js
+++ 10-Axios-WeatherNPM/JSON-start/js/scripts.js
@@ -1,4 +1,4 @@
-
+const apiURL= "//api.openweathermap.org/data/2.5/weather?id=5419384&appid=b347cf40f5ceaeb40d7a64655b671d34&units=imperial"
 fetch ("testdata.json")
 .then((mydata) => mydata.json())
 .then((mydata) => {
@@ -29,5 +29,15 @@ fetch (apiURL)
     document.getElementById("place").innerHTML=weatherInfo.name; 
     document.getElementById("currentTemp").innerHTML=weatherInfo.main.temp;
     document.getElementById("feels").innerHTML=weatherInfo.main.feels_like; 
-    document.getElementById("windSped").innerHTML=weatherInfo.wind.speed;  
+    document.getElementById("windSped").innerHTML=weatherInfo.wind.speed; 
+    // document.getElementById("humid").innerHTML=weatherInfo.main.humidity;  
+
+
+    const iconcode= weatherInfo.weather[0].icon; 
+    console.log(iconcode);
+    const icon_path = "//openweathermap.org/img/w/" + iconcode + ".png"; 
+    console.log(icon_path);
+
+    document.getElementById("weatherIcon").src= icon_path;
+    // document.getElementById("sun").innerHTML=weatherInfo.sys.sunrise; 
 }); 
